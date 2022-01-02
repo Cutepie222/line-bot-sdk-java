275781eae213e7f5d40db637cf97e2b69c38537e
Cutepie222 <96979340+Cutepie222@users.noreply.github.com>
2022-01-02 21:39:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'OQwbxm1yA4TAbHkuJJXMHOaMhIWJsgbQqIjlSUTi9NuzvP949SNDjxYsHcEyLTUp8talgLHJ9FboAnGgj15t+Ij1c3N1Z6HqItNBNd47y1e/0nrxRbuwLNrGP6SI63QMiS9FyyfYYVbwLIb6a5IyCwdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
